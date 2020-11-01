be15b27ef4d6d9f56f650c7ea711c5cb453512c1
DarioN555 <dario.novinc@gmail.com>
2020-11-01 17:34:25 +0100

    izmijenjen .gitignore i napravljen spring boot projekt

---
+++ backend/src/main/java/hr/fer/progi/raketa/onlinegalerija/OnlinegalerijaApplication.java
@@ -0,0 +1,14 @@
+package hr.fer.progi.raketa.onlinegalerija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinegalerijaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinegalerijaApplication.class, args);
+	}
+	
+	
+}
